f87cd1773b483adbaeb74e3fa2dc1f942ac3b99e
Akanbi AbdulAzeez <akanbiazeez117@gmail.com>
2025-04-02 22:53:39 +0100

    working on the highlights section

--- src/components/highlights.tsx
+++ src/components/highlights.tsx
@@ -1,8 +1,32 @@
-
+import gsap from"gsap";
+import {useGSAP} from '@gsap/react'
+import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./ui/videocarousel";
 
 const Highlights = () => {
+  useGSAP(() => {
+    gsap.to('#title', { opacity: 1, y:0, delay:2, })  
+    gsap.to('.link', { opacity: 1, y:0, delay:2,  duration: 2, stagger: 0.25})   
+},[])
   return (
-    <div>Highlights</div>
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-background">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end justify-between md:flex">
+        <h1 id="title" className="section-heading">Get the highlights.</h1>
+<div className="flex flex-wrap items-end gap-5">
+  <p className="link">
+    Watch the film
+    <img src={watchImg} alt="watch" className="ml-2"/> 
+    </p> 
+    <p className="link">
+    Watch the event
+    <img src={rightImg}  alt="watch" className="ml-2"/> 
+    </p> 
+    </div>       
+     </div>
+     <VideoCarousel/>
+      </div>
+    </section>
   )
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <header className=" w-full px-5 flex justify-between items-center py-7 sm:px-10 ">
         <nav className=" flex w-full screen-max-width  ">
-            <img src={appleImg} alt="Apple" width={14} height={18} />
+            <img src={appleImg} alt="Apple" width={14} height={18} className="md:fixed md:z-99"/>
 
             <div className="flex flex-1 justify-center max-sm:hidden ">
                 {navLists.map((nav, i) => (
@@ -14,7 +14,7 @@ const Navbar = () => {
                     </div>
                 ))}
             </div>
-            <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+            <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1 md:fixed md:right-30 md:z-99">
             <img src={searchImg} alt="Search" width={18} height={18} />
             <img src={bagImg} alt="bag" width={18} height={18} />
             </div>
