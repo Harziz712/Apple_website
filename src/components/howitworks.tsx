a79aae5b7cb051367fb4ab32fb0c2b1c4a812feb
Akanbi AbdulAzeez <akanbiazeez117@gmail.com>
2025-04-15 17:45:04 +0100

    howitworks added

--- src/components/howitworks.tsx
+++ src/components/howitworks.tsx
@@ -3,6 +3,7 @@ import { chipImg, frameImg, frameVideo } from "../utils"
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { useRef } from "react";
+import { animateWithGsap } from "../utils/animations";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -17,9 +18,15 @@ const HowItWorks = () => {
             opacity: 0, 
             scale: 2,
             duration: 2,
-            ease: "power2.out",
+            ease: "power2.inOut",
+            
         });
-    });
+        animateWithGsap('.g_fadeIn', {
+            y: 0, opacity: 1, duration: 1, ease: 'power2.inOut',}, { trigger: '.g_fadeIn' })
+     } ,[]);
+
+  
+
 
     return (
         <section className='common-padding'>
@@ -48,9 +55,34 @@ const HowItWorks = () => {
                     <p className="text-gray font-semibold text-center mt-3"> 
                         Honkai: Star Rail
                     </p>
+                    </div>
+                    <div className="hiw-text-container">
+                            <div className="flex-1 flex-center flex justify-center flex-col">
+                                <p className="hiw-text g_fadeIn">
+                                    A17 Pro is an entirely new class of iphone chip that delivers {' '} 
+                                    <span className='text-white'>
+                                  best graphics performance by far
+                                </span> {' '}
+                                </p>
+                         
+                            
+                                <p className="hiw-text g_fadeIn">
+                                  Mobile {' '}
+                                    <span className='text-white'>
+                                  games will look and feel so imersive,                             
+                                </span> {' '}
+                                with incredibly detailed environments and more realistic characters. And with industry-leading speed and efficiency. A17 Pro takes fast and runs with it
+                                 </p>
+                                 </div>
+                        <div className="flex flex-1 justify-center flex-col g_fadeIn">
+                            <p className="hiw-text">New</p>
+                            <p className="hiw-bigtext">Pro-class GPU</p>
+                            <p className="hiw-text">with 6 cores</p>
+                        </div>
+                               
+                        </div>
 
                 </div>
-            </div>
         </section>
     );
 };
